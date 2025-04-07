a96760d0b395ca48f0c00616e7832e98e9d5b7ba
GabrielBas <gabrielbasilio1230@gmail.com>
2025-04-07 19:59:50 -0300

    Balancing Weapon Fireball Finish

--- Assets/Scripts/Weapons/SpinWeapon.cs
+++ Assets/Scripts/Weapons/SpinWeapon.cs
@@ -13,17 +13,20 @@ public class SpinWeapon : Weapon
 
     public EnemyDamage damage;
 
-    // Start is called before the first frame update
+    [Header("Fireball Orbit Settings")]
+    public float orbitRadius = 2f;  // Controle do raio da órbita
+
+    private List<Transform> spawnedFireballs = new List<Transform>();
+
+
     void Start()
     {
         SetStats();
-
-        //UIController.instance.levelUpButtons[0].UpdateButtonDisplay(this);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Faz as fireballs girarem
         holder.rotation = Quaternion.Euler(0f, 0f, holder.rotation.eulerAngles.z + (rotateSpeed * Time.deltaTime * stats[weaponLevel].speed));
 
         spawnCounter -= Time.deltaTime;
@@ -31,36 +34,58 @@ public class SpinWeapon : Weapon
         {
             spawnCounter = timeBetweenSpawn;
 
-            //Instantiate(fireballToSpawn, fireballToSpawn.position, fireballToSpawn.rotation, holder).gameObject.SetActive(true);
-
-            for (int i = 0; i < stats[weaponLevel].amount; i++)
-            {
-                float rot = (360f / stats[weaponLevel].amount) * i;
-
-                Instantiate(fireballToSpawn, fireballToSpawn.position, Quaternion.Euler(0f, 0f, rot), holder).gameObject.SetActive(true);
-            }
-
+            SpawnFireballsAroundPlayer();
         }
 
-        if (statsUpdated == true)
+        if (statsUpdated)
         {
             statsUpdated = false;
-
             SetStats();
+            SpawnFireballsAroundPlayer(); // Atualiza o círculo de fireballs
+        }
+    }
+
+    void SpawnFireballsAroundPlayer()
+    {
+        int amount = Mathf.RoundToInt(stats[weaponLevel].amount);
+        float radius = orbitRadius * stats[weaponLevel].range;
+
+        // Instancia fireballs adicionais se necessário
+        while (spawnedFireballs.Count < amount)
+        {
+            Transform fireball = Instantiate(fireballToSpawn, transform.position, Quaternion.identity, holder);
+            fireball.gameObject.SetActive(true);
+            spawnedFireballs.Add(fireball);
         }
 
+        // Se houver mais do que o necessário, desativa ou remove extras
+        while (spawnedFireballs.Count > amount)
+        {
+            Destroy(spawnedFireballs[spawnedFireballs.Count - 1].gameObject);
+            spawnedFireballs.RemoveAt(spawnedFireballs.Count - 1);
+        }
+
+        // Posiciona todas as fireballs igualmente ao redor do player
+        for (int i = 0; i < spawnedFireballs.Count; i++)
+        {
+            float angle = (360f / spawnedFireballs.Count) * i;
+            float angleRad = angle * Mathf.Deg2Rad;
+
+            Vector3 offset = new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad), 0f) * radius;
+            Vector3 spawnPos = transform.position + offset;
+
+            spawnedFireballs[i].position = spawnPos;
+        }
     }
+
+
 
     public void SetStats()
     {
         damage.damageAmount = stats[weaponLevel].damage;
-
         transform.localScale = Vector3.one * stats[weaponLevel].range;
-
         timeBetweenSpawn = stats[weaponLevel].timeBetweenAttacks;
-
         damage.lifeTime = stats[weaponLevel].duration;
-
         spawnCounter = 0f;
     }
 }

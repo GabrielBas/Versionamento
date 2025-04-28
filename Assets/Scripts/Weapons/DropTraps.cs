e1f7e351ad0ff1ae8ca9436423315482e9e657bc
GabrielBas <gabrielbasilio1230@gmail.com>
2025-04-28 19:55:58 -0300

    Drop Traps Weapon Finish

---
+++ Assets/Scripts/Weapons/DropTraps.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropTraps : MonoBehaviour
+{
+    public float throwPower;
+    public Rigidbody2D theRB;
+    public float rotateSpeed;
+
+    //private TrailRenderer trail;       // Referência ao Trail Renderer
+    //private bool isDrawing = false;    // Indica se está desenhando ou não
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB.velocity = new Vector2(Random.Range(-throwPower, throwPower), throwPower);
+
+        //trail = GetComponent<TrailRenderer>();
+        //trail.emitting = false; // Inicializa o Trail Renderer desativado
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0f, 0f, transform.rotation.eulerAngles.z + (rotateSpeed * 360f * Time.deltaTime * Mathf.Sign(theRB.velocity.x)));
+
+        //// Detecta o clique do botão direito para alternar o desenho
+        //if (Input.GetMouseButtonDown(1))  // Botão direito do mouse
+        //{
+        //    isDrawing = !isDrawing;  // Alterna o estado de desenho
+
+        //    if (isDrawing)
+        //    {
+        //        //trail.Clear();  // Limpa o rastro anterior
+        //        trail.emitting = true;  // Inicia o desenho
+        //    }
+        //    else
+        //    {
+        //        trail.emitting = false;  // Para o desenho
+        //    }
+        //}
+
+    }
+}
---
+++ Assets/Scripts/Weapons/RainWeapon.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainWeapon : Weapon
+{
+    public EnemyDamage damager;
+    public GameObject weaponPrefab; // Prefab da arma a ser lançada
+
+    private float throwCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetStats();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (statsUpdated == true)
+        {
+            statsUpdated = false;
+            SetStats();
+        }
+
+        throwCounter -= Time.deltaTime;
+        if (throwCounter <= 0)
+        {
+            throwCounter = stats[weaponLevel].timeBetweenAttacks;
+
+            for (int i = 0; i < stats[weaponLevel].amount; i++)
+            {
+                if (weaponPrefab != null)
+                {
+                    // Se um prefab de arma estiver definido, instancie-o
+                    GameObject thrownWeapon = Instantiate(weaponPrefab, transform.position, transform.rotation);
+                    thrownWeapon.SetActive(true);
+                }
+                else
+                {
+                    // Caso contrário, use o comportamento padrão com damager
+                    Instantiate(damager, damager.transform.position, damager.transform.rotation).gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+
+    void SetStats()
+    {
+        damager.damageAmount = stats[weaponLevel].damage;
+        damager.lifeTime = stats[weaponLevel].duration;
+
+        damager.transform.localScale = Vector3.one * stats[weaponLevel].range;
+
+        throwCounter = 0f;
+    }
+}
